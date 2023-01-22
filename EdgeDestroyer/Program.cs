0f83b267f3ae7d0eaa6391c1926d4a2011770f04
Scrambled Onion <scrambledonion2222@gmail.com>
2023-01-22 01:26:34 -0500

    Add project files.

---
+++ EdgeDestroyer/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EdgeDestroyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Public\\Desktop\\Microsoft Edge.lnk";
+            if(File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            return;
+        }
+    }
+}
